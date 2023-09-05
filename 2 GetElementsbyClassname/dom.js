de4d6bd75e4b2be950a1e2505cd394f0603adccc
ayushpasi <pasiayush999@gmail.com>
2023-09-05 12:19:37 +0530

    task2 getElementByClassName added

---
+++ 2 GetElementsbyClassname/dom.js
@@ -0,0 +1,18 @@
+//EXAMIN THE DOCUMENT OBJECT//
+// console.dir(document);
+
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// console.log(document.doctype);
+// console.log(document.body);
+// console.log(document.all);
+const headTitle = document.getElementById("main-header");
+//headTitle.textContent = "hello";//style not matters
+//headTitle.innerText = "Hello";//style matters
+//headTitle.innerHTML = "<h3>H3</h3>";
+//headTitle.style.borderBottom = "5px solid black";
+
+const addItem = document.getElementById("addItem");
+addItem.style.fontWeight = "bold";
+addItem.style.color = "green";
