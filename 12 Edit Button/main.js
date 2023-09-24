8a64de23a8b0af7fd8ca7b9dca1acc8ba480d05e
ayushpasi <pasiayush999@gmail.com>
2023-09-24 22:08:08 +0530

    updated Book appoBooking appointment App added

--- 12 Edit Button/main.js
+++ 12 Edit Button/main.js
@@ -29,68 +29,20 @@ function saveData(e) {
 
   // Store the JSON data in local storage with the email as the key
   localStorage.setItem(email, data);
-
-  // Create a new list item element
-  var li = document.createElement("li");
-
-  // Add a class "item" to the list item
-  li.className = "item";
-
-  // Create a text node with user data and append it to the list item
-  li.appendChild(
-    document.createTextNode(name + "  " + email + "  " + mobile + "  ")
-  );
-
-  // Create a delete button element
-  var btn = document.createElement("button");
-
-  // Add a class "delete" to the button
-  btn.className = "delete";
-
-  // Add text "delete" to the button
-  btn.appendChild(document.createTextNode("delete"));
-
-  // Add a click event listener to the delete button
-  btn.addEventListener("click", removeUser);
-
-  // Create an edit button element
-  var edit = document.createElement("button");
-
-  // Add a class "edit" to the button
-  edit.className = "edit";
-
-  // Add text "edit" to the button
-  edit.appendChild(document.createTextNode("edit"));
-
-  // Add a click event listener to the edit button
-  edit.addEventListener("click", editData);
-
-  // Append the edit and delete buttons to the list item
-  li.appendChild(edit);
-  li.appendChild(btn);
-
-  // Append the list item to the "users" ul
-  user.appendChild(li);
-
-  // Log the data stored in local storage for the given email
-  console.log(localStorage.getItem(email));
-
-  // Function to remove a user when the delete button is clicked
-  function removeUser(e) {
-    if (confirm("Are you sure?")) {
-      var li = e.target.parentNode;
-      user.removeChild(li);
-      var emailDelete = findEmail(li);
-      localStorage.removeItem(emailDelete[1].trim());
-    }
-  }
-
-  // Clear the form fields after submission
   document.getElementById("name").value = "";
   document.getElementById("email").value = "";
   document.getElementById("mobile").value = "";
+  showData(dataObj);
 }
-
+// Function to remove a user when the delete button is clicked
+function removeUser(e) {
+  if (confirm("Are you sure?")) {
+    var li = e.target.parentNode;
+    user.removeChild(li);
+    var emailDelete = findEmail(li);
+    localStorage.removeItem(emailDelete[1].trim());
+  }
+}
 // Function to split and extract the name, email, and mobile from the list item
 function findEmail(li) {
   var text = li.textContent;
@@ -116,4 +68,64 @@ function editData(e) {
   document.getElementById("name").value = name;
   document.getElementById("email").value = email;
   document.getElementById("mobile").value = mobile;
+}
+
+function showData(obj) {
+  // Create a new list item element
+  var li = document.createElement("li");
+
+  // Add a class "item" to the list item
+  li.className = "item";
+
+  // Create a text node with user data and append it to the list item
+  li.appendChild(
+    document.createTextNode(
+      obj.name + "  " + obj.email + "  " + obj.mobile + "  "
+    )
+  );
+
+  // Create a delete button element
+  var btn = document.createElement("button");
+
+  // Add a class "delete" to the button
+  btn.className = "delete";
+
+  // Add text "delete" to the button
+  btn.appendChild(document.createTextNode("delete"));
+
+  // Add a click event listener to the delete button
+  btn.onclick = () => {
+    user.removeChild(li);
+    localStorage.removeItem(obj.email);
+  };
+
+  // Create an edit button element
+  var edit = document.createElement("button");
+
+  // Add a class "edit" to the button
+  edit.className = "edit";
+
+  // Add text "edit" to the button
+  edit.appendChild(document.createTextNode("edit"));
+
+  // Add a click event listener to the edit button
+  edit.onclick = () => {
+    user.removeChild(li);
+    localStorage.removeItem(obj.email);
+    document.getElementById("name").value = obj.name;
+    document.getElementById("email").value = obj.email;
+    document.getElementById("mobile").value = obj.mobile;
+  };
+
+  // Append the edit and delete buttons to the list item
+  li.appendChild(edit);
+  li.appendChild(btn);
+
+  // Append the list item to the "users" ul
+  user.appendChild(li);
+
+  // Log the data stored in local storage for the given email
+  console.log(localStorage.getItem(email));
+
+  // Clear the form fields after submission
 }
